2226c9e3d7fd1131ca33027fe1b63e23e54e6ed3
Wha-The <47490607+Wha-The@users.noreply.github.com>
2022-12-24 11:22:11 +0800

    Create filesystem_simulation.lua

---
+++ umodules/filesystem_simulation.lua
@@ -0,0 +1,37 @@
+local get_parts_of_path = function(path)
+    return string.split(path, "/")
+end
+local derive_parent = function(path)
+    local parts = get_parts_of_path()
+    parts[#parts] = nil
+    return table.concat(parts, "/")
+end
+local derive_end = function(path)
+    local parts = get_parts_of_path()
+    return parts[#parts]
+end
+shared.filesystem = shared.filesystem or {}
+readfile = function(path)
+    local current = shared.filesystem
+    for _, part in pairs(get_parts_of_path(path)) do
+        if not current[part] then
+            return false
+        end
+        current = current[part]
+    end
+    return current
+end
+
+isfile = function(path)
+    if path == "" then return true end
+    return readfile(path) ~= false
+end
+isfolder = isfile
+writefile = function(path, data)
+    if isfolder(derive_parent(path)) then
+        readfile(derive_parent(path))[derive_end(path)] = data
+    end
+end
+makefolder = function(path)
+    return writefile(path, {})
+end
